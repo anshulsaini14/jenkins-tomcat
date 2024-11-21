cf0119ca69dc9a18319d6892a4e01638cb949a5e
anshulsaini14 <148077920+anshulsaini14@users.noreply.github.com>
2024-11-21 16:09:44 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "This is Jenkins Tomcat.";
+        return "This is Jenkins Tomcat in CI/CD.";
     }
 
 }
